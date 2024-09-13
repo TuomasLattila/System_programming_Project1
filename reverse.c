9041941257c2dfe0c1d67f0c48605b68b7847841
TuomasLattila <tuomas.lattila@outlook.com>
2024-09-13 14:06:27 +0300

    Commented the code.

--- reverse.c
+++ reverse.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//struct for linked list containing the rows
 typedef struct rows {
     char *aRow;
     struct rows *nextRow;
@@ -13,9 +14,6 @@ ROWS *addToList(ROWS *pStart, char *pRow);
 void writeInReversedOrder(ROWS *pStart, FILE *pFileName);
 ROWS *freeMemory(ROWS *pStart);
 
-/*At the moment the code expects one or two commandline argument which
-are the names of the input file and the output file.*/
-
 int main(int argc, char *argv[]) {
     
     if (argc > 3) { //Tests if too many arguments given
@@ -23,7 +21,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     };
 
-    ROWS *pStart = NULL;
+    ROWS *pStart = NULL; //start of the linked list
 
     switch (argc) {
         case 1: //No input or output file given
@@ -35,23 +33,23 @@ int main(int argc, char *argv[]) {
             writeInReversedOrder(pStart, stdout);
             break;
         case 3: //Input and output files given
-            if (strcmp(argv[1], argv[2]) == 0) {
+            if (strcmp(argv[1], argv[2]) == 0) { //test if the input and output files are the same
                 fprintf(stderr, "Input and output file must differ\n");
                 exit(1);
             };
             pStart = readFile(pStart, argv[1]);
             FILE *file;
             if ((file = fopen(argv[2], "w")) == NULL)    {
-            fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
-            exit(1);
+                fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
+                exit(1);
             };
-            writeInReversedOrder(pStart, file);
+            writeInReversedOrder(pStart, file); //write to the output file
             fclose(file);
             break;
         default:
             break;
     };
-    pStart = freeMemory(pStart);
+    pStart = freeMemory(pStart); //free the linkedlist
     return 0;
 };
 
@@ -61,29 +59,29 @@ ROWS *readFile(ROWS *pStart, char *pFileName) {
     size_t len = 0;
     __ssize_t read;
 
-    if ((file = fopen(pFileName, "r")) == NULL)    {
+    if ((file = fopen(pFileName, "r")) == NULL)    { //opens the input file
         fprintf(stderr, "error: cannot open file '%s'\n", pFileName);
         exit(1);
     };
 
-    while((read = getline(&pRow, &len, file)) != -1) {
+    while((read = getline(&pRow, &len, file)) != -1) { //reads lines and appends them to the linked list
         pStart = addToList(pStart, pRow);
     };
     fclose(file);
-    free(pRow);
+    free(pRow); 
     return pStart;
 }
-// this is where I left last time
+
 ROWS *readInput(ROWS *pStart) {
     char *pRow = NULL;
     size_t len = 0;
     __ssize_t read;
 
     printf("Write multiple lines (end: ctrl+D):\n\n");
-    while ((read = getline(&pRow, &len, stdin)) != -1) {
+    while ((read = getline(&pRow, &len, stdin)) != -1) { //read from the stdin and append row to list
         pStart = addToList(pStart, pRow);
     };
-    if (feof(stdin)) {
+    if (feof(stdin)) { //test if writing was ended
         printf("\nWriting ended.\n\nLines in reversed:\n\n");
     } else if (ferror(stdin)) {
         fprintf(stderr, "Reading from stdin failed");
@@ -98,12 +96,12 @@ ROWS *readInput(ROWS *pStart) {
 ROWS *addToList(ROWS *pStart, char *pRow)   {
     ROWS *pNew = NULL;
 
-    if ((pNew = (ROWS *)malloc(sizeof(ROWS))) == NULL)  {
+    if ((pNew = (ROWS *)malloc(sizeof(ROWS))) == NULL)  { // allocates memroy for new ROW struct
         fprintf(stderr, "malloc failed\n");
         exit(1);
     };
 
-    if ((pNew->aRow = (char *)malloc(strlen(pRow)+1)) == NULL)  {
+    if ((pNew->aRow = (char *)malloc(strlen(pRow)+1)) == NULL)  { // allocates memory for the row size and adds it to the new ROW
         fprintf(stderr, "malloc failed\n");
         exit(1);
     };
@@ -114,7 +112,7 @@ ROWS *addToList(ROWS *pStart, char *pRow)   {
     if (pStart == NULL) {
         pStart = pNew;
     } else {
-        pNew->nextRow = pStart;
+        pNew->nextRow = pStart; //appends the new row to the start of the list (this way the rows are automatically in reversed order)
         pStart = pNew;
     }
     return pStart;
@@ -124,13 +122,13 @@ ROWS *addToList(ROWS *pStart, char *pRow)   {
 void writeInReversedOrder(ROWS *pStart, FILE *pFileName) {
     ROWS *ptr = pStart;
     while (ptr != NULL) {
-    fprintf(pFileName, "%s", ptr->aRow);
-    ptr = ptr->nextRow;
+        fprintf(pFileName, "%s", ptr->aRow); //print the linked list
+        ptr = ptr->nextRow;
     }
     return;
 };
 
-ROWS *freeMemory(ROWS *pStart)  {
+ROWS *freeMemory(ROWS *pStart)  { //frees the linked list
     ROWS *ptr = pStart;
     while (ptr != NULL) {
         pStart = ptr->nextRow;
